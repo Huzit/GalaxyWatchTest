564c527180e91f09c98ee5d88ea0d35463d25696
박상환 <hwan4789@gmail.com>
2023-05-04 16:58:44 +0900

    커밋 완료

--- app/src/main/java/com/weather/weartest/MainActivity.kt
+++ app/src/main/java/com/weather/weartest/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.weather.weartest
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.wear.ambient.AmbientModeSupport
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
 import com.weather.weartest.databinding.ActivityMainBinding
 import com.weather.weartest.retrofit.EndPoint
 import com.weather.weartest.retrofit.RetrofitIntance
@@ -24,15 +36,59 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     lateinit var heartRateSensor: Sensor
     lateinit var offBodySensor: Sensor
     lateinit var test: Sensor
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    lateinit var locationRequest: LocationRequest
     val sensorCode: Int = 69635
 
+    val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                // Precise location access granted.
+            }
+            permissions.getOrDefault(android.Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // Only approximate location access granted.
+            } else -> {
+            // No location access granted.
+        }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //위치 권한
+        locationPermissionRequest.launch(arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION))
+        //권한 체크
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }else{
+            //위치 설정
+            createLocationRequest()
+            //마지막 위치 찾기
+            findLastLocation()
+            //콜백 정의
+            getLocationCallback()
+            //업데이트 시작
+            startLocationUpdates(locationRequest, locationCallback)
+        }
+
+
         requestPermissions(arrayOf("android.permission.BODY_SENSORS"), 1)
+
         ambientController = AmbientModeSupport.attach(this)
 
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
@@ -61,6 +117,57 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         })
     }
+    //GPS 센서 여부
+    fun getGps() = packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS)
+
+    // 지난 위치값 가져오기
+    fun findLastLocation(){
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            location: Location? ->
+            binding.test.text = "위치 정보 : ${location.toString()}"
+            Log.d("location", "$location")
+        }
+    }
+    //위치 설정
+    fun createLocationRequest(){
+        locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+
+        task.apply {
+            addOnSuccessListener { task ->
+                Log.d("Success", "$task")
+            }
+
+            addOnFailureListener{ task ->
+                Log.d("Faild", "$task")
+            }
+        }
+    }
+
+    fun startLocationUpdates(locationRequest: LocationRequest, locationCallback: LocationCallback){
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+    }
+
+    fun getLocationCallback(){
+        locationCallback = object : LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                locationResult ?: return
+                locationResult.locations.forEach {
+                    Log.d("locations", "$it")
+                }
+            }
+        }
+    }
+
     override fun onSensorChanged(event: SensorEvent?) {
         when(event!!.sensor.type){
             Sensor.TYPE_HEART_RATE -> {
@@ -94,6 +201,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onPause() {
         super.onPause()
         sensorManager.unregisterListener(this)
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
 }
