235a4746e82839e30e2ac0057f6fc45056accbb7
INCONUS <inconus.kr@gmail.com>
2023-08-04 18:03:24 +0900

    보로미터 테스트 중

--- app/src/main/java/com/weather/weartest/MainActivity.kt
+++ app/src/main/java/com/weather/weartest/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.weather.weartest
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.hardware.Sensor
+import android.hardware.SensorDirectChannel
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
@@ -11,17 +11,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.room.Room
-import com.weather.weartest.database.AppDatabase
-import com.weather.weartest.database.LocationInfo
 import com.weather.weartest.databinding.ActivityMainBinding
-import com.weather.weartest.retrofit.EndPoint
-import com.weather.weartest.retrofit.RetrofitIntance
-import com.weather.weartest.retrofit.TongSinTestResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
@@ -31,8 +21,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     lateinit var heartRateSensor: Sensor //심박 센서
     lateinit var offBodySensor: Sensor   //착용감지센서
     lateinit var test: Sensor
+    val hrCode = Sensor.TYPE_HEART_RATE
     lateinit var findLocation: FindLocation
-    val sensorCode: Int = 69635
+    val sensorCode = 6
 
     val locationPermissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
@@ -45,7 +36,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 // Only approximate location access granted.
             } else -> {
             // No location access granted.
-        }
+            }
         }
     }
 
@@ -54,12 +45,10 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         /**/
-//        locationPermissionRequest.launch(arrayOf(
-//
-//            )
-//        )
-        requestPermissions(arrayOf("android.permission.BODY_SENSORS", android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+        getSensorList()
 
+        requestPermissions(arrayOf("android.permission.BODY_SENSORS", android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+        requestPermissions(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),1001)
         findLocation = FindLocation(this, binding)
         findLocation.initDatabase()
         initSensor()
@@ -69,27 +58,40 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 //        ambientController = AmbientModeSupport.attach(this)
 
         //위치 설정
-        findLocation.createLocationRequest()
+//        findLocation.createLocationRequest()
         //마지막 위치 찾기
-        findLocation.findLastLocation()
+//        findLocation.findLastLocation()
         //콜백 정의
         //위치 업데이트 요청
-        startForegroundService(Intent(this, FindService::class.java))
+//        startForegroundService(Intent(this, FindService::class.java))
     }
 
     //센서 정의
     fun initSensor(){
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)
+
+        heartRateSensor = sensorManager.getDefaultSensor(hrCode, true)
+
         offBodySensor = sensorManager.getDefaultSensor(34)
         test = sensorManager.getDefaultSensor(sensorCode)
+    }
+
+    fun getSensorList(){
+        val mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        val sensors = ArrayList<String>()
+
+        mSensorManager.getSensorList(Sensor.TYPE_AMBIENT_TEMPERATURE).forEach {
+            sensors.add("${it.name} ${it.type} ${it.isDirectChannelTypeSupported(SensorDirectChannel.TYPE_HARDWARE_BUFFER)} ${it.isWakeUpSensor}")
+        }
+
+        sensors.forEach{ n -> Log.d("sensorList", n)}
     }
 
     //GPS 센서 여부
     fun getGps() = packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS)
 
     //통신 테스트
-    fun retrofitTest(){
+/*    fun retrofitTest(){
         val service = RetrofitIntance().testTongsin().create(EndPoint::class.java)
         service.httpTest().enqueue(object : Callback<List<TongSinTestResponse>> {
             override fun onResponse(
@@ -106,12 +108,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             }
 
         })
-    }
+    }*/
 
     //센서 이벤트
     override fun onSensorChanged(event: SensorEvent?) {
+        Log.d("SensorChangedEvent", event!!.sensor.name)
+
         when(event!!.sensor.type){
-            Sensor.TYPE_HEART_RATE -> {
+            hrCode -> {
                 val heartRate = event.values?.get(0) ?: 0
                 Log.d("센서 값 : ", "${heartRate}")
                 binding.hrTv.text = "심박수 : $heartRate "
@@ -123,14 +127,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             }
             sensorCode -> {
                 val t = event.values[0] ?: 0
-                event.sensor.name
-                Log.d("테스트 값 : ", "$t")
-                binding.test.text = "테스트 값 : \n${event.sensor.name}\n$t"
+
+                binding.test.text = "보로미터 값 : \n${event.sensor.name}\n$t"
             }
         }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d("AccuracyChangedEvent", sensor!!.name)
     }
 
     override fun onResume() {
@@ -142,7 +146,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 //        }
         sensorManager.registerListener(this, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL)
         sensorManager.registerListener(this, offBodySensor, SensorManager.SENSOR_DELAY_NORMAL)
-        sensorManager.registerListener(this, test, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, test, SensorManager.SENSOR_DELAY_UI)
 
 //        if(findLocation.requestingLocationUpdates){
             findLocation.startLocationUpdates()
@@ -159,5 +163,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onDestroy()
         findLocation.stopLocationUpdates()
         findLocation.getLocationDatabase()
+
     }
 }
